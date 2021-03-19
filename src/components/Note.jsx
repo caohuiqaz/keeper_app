cedd894dfcb2d487e40def02038338d13b1b0790
Hui Cao <caohuiqaz@gmail.com>
2021-03-19 11:27:42 +1100

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>
+        note 1
+      </h1>
+      <p>
+        hello
+      </p>
+    </div>
+  );
+}
+
+export default Note;
